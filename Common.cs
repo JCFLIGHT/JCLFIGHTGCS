56cc2fc5aca82701e649dbe9a9c1726acd5eb58d
JuliooCesarMDM <juliomatias123@outlook.com>
2020-11-16 02:40:11 -0300

    consertar a posição do marker

--- Common.cs
+++ Common.cs
@@ -74,12 +74,10 @@ namespace JCFLIGHTGCS
 
     public class CreateGMapMarker : GMapMarker
     {
-        static readonly Size SizeSt = new Size(Properties.Resources.marker_01.Width, Properties.Resources.marker_01.Height);
         private byte WPNumber;
         public CreateGMapMarker(PointLatLng Point, byte ID)
             : base(Point)
         {
-            Size = SizeSt;
             WPNumber = ID;
         }
 
@@ -89,14 +87,14 @@ namespace JCFLIGHTGCS
             Matrix temp = g.Transform;
             g.TranslateTransform(LocalPosition.X, LocalPosition.Y);
             pic = Properties.Resources.marker_01;
-            g.DrawImageUnscaled(pic, pic.Width / -2 - 4, -pic.Height - 10);
+            g.DrawImageUnscaled(pic, pic.Width / -2 - 1, -pic.Height + 3);
             Font drawFont = new Font(FontFamily.GenericMonospace, 9.0F, FontStyle.Bold);
             SolidBrush drawBrush = new SolidBrush(Color.White);
             drawBrush.Color = Color.Yellow;
-            g.DrawString("WP", drawFont, drawBrush, -12, -53);
+            g.DrawString("WP", drawFont, drawBrush, -10, -54);
             drawBrush.Color = Color.White;
-            if (WPNumber < 10) g.DrawString(String.Format("{0:0}", WPNumber), drawFont, drawBrush, -9, -42);
-            if (WPNumber < 100 && WPNumber > 9) g.DrawString(String.Format("{0:0}", WPNumber), drawFont, drawBrush, -12, -43);
+            if (WPNumber < 10) g.DrawString(String.Format("{0:0}", WPNumber), drawFont, drawBrush, -6, -37);
+            if (WPNumber < 100 && WPNumber > 9) g.DrawString(String.Format("{0:0}", WPNumber), drawFont, drawBrush, -10, -37);
             if (WPNumber > 100) g.DrawString(String.Format("{0:0}", WPNumber), drawFont, drawBrush, -12, -40);
             g.Transform = temp;
 
